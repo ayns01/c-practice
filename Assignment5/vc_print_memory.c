bbb3572329784ca3d091353a736a4a00e89299e9
ayns <peenats1203@gmail.com>
2019-02-10 01:44:34 -0800

    Add vc_print_memory.c, Delete .gitignore

--- Assignment5/vc_print_memory.c
+++ Assignment5/vc_print_memory.c
@@ -6,9 +6,35 @@
 
 void *vc_print_memory(void *addr, unsigned int size) {
 
-    addr = &addr;
+    const unsigned char* pc = addr;
+    int i;
+    size_t printed = 0;
+    char* str = addr;
 
-    return addr;
+    for (i=0; i<size; ++i)
+    {
+        int g;
+        g = (*(pc + i) >> 4) & 0xf;
+        g += g >= 10 ? 'a' - 10 : '0';
+        putchar(g);
+        printed++;
+
+        g = *(pc + i) & 0xf;
+        g += g >= 10 ? 'a' - 10 : '0';
+        putchar(g);
+        printed++;
+
+        if (printed % 32 == 0) {
+            putchar(' ');
+            for (int j = 0; j < 16; ++j)
+            {
+                putchar(*str);
+                str++;
+            }
+            putchar('\n');
+        }
+        else if (printed % 4 == 0) putchar(' ');
+    }
 
 }
 
